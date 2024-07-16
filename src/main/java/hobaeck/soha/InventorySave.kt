d5ab5bb1958e85c3978507f3b6a5b7fa18bbe416
yudonghun <reona7140@naver.com>
2024-07-16 21:34:21 +0900

    메인클래스만 코틀린으로 변경

--- src/main/java/hobaeck/soha/InventorySave.kt
+++ src/main/java/hobaeck/soha/InventorySave.kt
@@ -1,38 +1,34 @@
-package hobaeck.soha;
+package hobaeck.soha
 
-import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
 /**
  * 1. 6,1 ~ 6,4 / 6,6 ~ 6,9 에 검은색 스테인글래스 팬 아이템 넣고, 6,5 에는 저장하는 버튼? 초록색이나 빨간색 스테인 글래스 넣고
- *  6,5 클릭하면 저장, 1,1 ~ 5,9 의 아이템들만 저장됨.
+ * 6,5 클릭하면 저장, 1,1 ~ 5,9 의 아이템들만 저장됨.
  *
  * 2. 코틀린으로 전환 << *****
  */
-public final class InventorySave extends JavaPlugin {
+class InventorySave : JavaPlugin() {
 
-    // singleton
-    private static InventorySave instance;
+    private val blackListInventoryManager = BlackListInventoryManager()
 
-    public static InventorySave getInstance() {
-        return instance;
+    override fun onEnable() {
+        instance = this
+        server.pluginManager.registerEvents(InventorySaveListener(blackListInventoryManager, this), this)
+        val inventorySaveCommand = InventorySaveCommand(blackListInventoryManager)
+        Objects.requireNonNull(getCommand("인벤세이브"))?.setExecutor(inventorySaveCommand)
+        Objects.requireNonNull(getCommand("인벤세이브"))?.tabCompleter = inventorySaveCommand
     }
 
-    private BlackListInventoryManager blackListInventoryManager = new BlackListInventoryManager();
-
-    @Override
-    public void onEnable() {
-//        saveDefaultConfig();
-        instance = this;
-        getServer().getPluginManager().registerEvents(new InventorySaveListener(blackListInventoryManager, this), this);
-        InventorySaveCommand inventorySaveCommand = new InventorySaveCommand(blackListInventoryManager);
-        Objects.requireNonNull(getCommand("인벤세이브")).setExecutor(inventorySaveCommand);
-        Objects.requireNonNull(getCommand("인벤세이브")).setTabCompleter(inventorySaveCommand);
+    override fun onDisable() {
+        blackListInventoryManager.saveBlackListItems()
     }
 
-    @Override
-    public void onDisable() {
-        blackListInventoryManager.saveBlackListItems();
+    companion object {
+        // singleton
+        @JvmStatic
+        var instance: InventorySave? = null
+            private set
     }
 }
